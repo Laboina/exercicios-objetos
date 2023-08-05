3ff6c29c9a3866f3819929174a004a2d3148eb08
Larissa Boina <boina.larissa1@gmail.com>
2023-08-05 11:17:38 -0300

    criado cadastro usuario exercicio cubos

---
+++ exercicio1.js
@@ -0,0 +1,22 @@
+//cadastro do usuario
+
+const endereco = {
+    rua: "Rua Amélia Ferreira",
+    numero: 57,
+    complemento: 1,
+    bairro: "Jardim Caiçara",
+    cep: 28910280,
+    cidade: "Cabo Frio",
+    estado: "Rio de Janeiro",
+    pais: "Brasil"
+}
+
+const usuario = {
+    nome: "Larissa",
+    email: "larissaboina@gmail.com",
+    telefone: 71991371241,
+    data_nascimento: 11.05, 
+    endereco
+}
+
+console.log(usuario); 
